579e0f1b15d31b614890a15eb140fd93b53154a6
Xoroxa <34523791+Xoroxa@users.noreply.github.com>
2024-05-19 20:11:04 -0400

    Update simple.rs

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,19 +1,9 @@
-// use snips_nlu_lib::ontology::{IntentClassifierResult, IntentParserAlternative, Slot};
-use snips_nlu_lib::ontology::IntentParserResult;
 use snips_nlu_lib::SnipsNluEngine;
-// use snips_nlu_ontology::IntentParserResult;
+use serde_json;
 
 pub struct NLU {
     inner: SnipsNluEngine,
 }
-
-// pub struct IntentParserResult {
-//     pub input: String,
-//     pub intent: IntentClassifierResult,
-//     pub slots: Vec<Slot>,
-//     #[serde(default)]
-//     pub alternatives: Vec<IntentParserAlternative>,
-// }
 
 impl NLU {
     pub fn new(engine_dir: &str) -> Self {
@@ -23,12 +13,10 @@ impl NLU {
         }
     }
 
-    pub async fn parse_intent(&self, input: String) -> snips_nlu_lib::ontology::IntentParserResult {
-
-        self.inner.parse_with_alternatives(input.trim(), None, None, 0, 0).unwrap()
-        // let result = self.inner.parse_with_alternatives(input.trim(), None, None, 0, 0).unwrap();
-        // let result_json = serde_json::to_string_pretty(&result).unwrap();
-        // serde_json::from_str(&result_json).unwrap()
+    pub async fn parse_intent(&self, input: String) -> String {
+        let result = self.inner.parse_with_alternatives(input.trim(), None, None, 0, 0).unwrap();
+        let result_json = serde_json::to_string_pretty(&result).unwrap();
+        serde_json::from_str(&result_json).unwrap()
     }
 }
 
